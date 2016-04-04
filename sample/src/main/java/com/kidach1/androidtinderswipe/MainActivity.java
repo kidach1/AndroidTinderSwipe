f941d1ad2ceb410058fd44b6f88afafa106b16fb
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2016-04-04 10:58:03 +0900

    remove unneeded

--- sample/src/main/java/com/kidach1/androidtinderswipe/MainActivity.java
+++ sample/src/main/java/com/kidach1/androidtinderswipe/MainActivity.java
@@ -2,7 +2,6 @@ package com.kidach1.androidtinderswipe;
 
 import android.content.DialogInterface;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
